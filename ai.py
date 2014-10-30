cf9c87815eff2c0a34e7ffa75121fda765bd36c0
Caleb Nelson <calebnelson@gmail.com>
2014-10-30 07:53:29 +0000

    added left ai

--- ai.py
+++ ai.py
@@ -7,7 +7,6 @@ import re
 Directions = [ 'up', 'right', 'down', 'left' ]
 
 def handler(req):
-        req.log_error('handler')
         req.content_type = 'text/text'
 
 	# get url parameters, and parse into dictionary
@@ -21,18 +20,54 @@ def handler(req):
         # clean up the board by turning runs of spc comma bar into comma
 	board = re.sub('[ ,]+', ',', queryDct['board'][0]) if 'board' in queryDct else ''
 	# split into array
-	board = board.split(',')
-
+	board = map(int, board.split(','))
+	grid = boardToGrid(board)
 	# Log the request (ToDo)
 
 	# Dispatch to the specified AI (ToDo: only supports rand for now)
-        move = AIrand(board, scr, arg)
+        if ai == 'left':
+		move = AIleft(req, grid, scr, arg)
+	else:
+		move = AIrand(req, grid, scr, arg)
 
 	# Write the response to the client
         req.send_http_header()
         req.write(move['dir'] + '\n' + move['msg'] + '\n')
         return apache.OK
 
-def AIrand(board, score, arg):
+def boardToGrid(board):
+	grid = []
+	for row in range(0, 4):
+		grid.append([])
+		for col in range(0, 4):
+			grid[row].append(board[row*4+col])
+	return grid
+
+def gridToString(grid):
+	return "[" + ','.join(map(str, grid[0])) + " | " + ','.join(map(str, grid[1])) + " | " + ','.join(map(str, grid[2])) + " | " + ','.join(map(str, grid[3])) + "]"  
+
+def AIrand(req, grid, score, arg):
 	dir = Directions[randint(0, 3)]
-	return {'dir':dir, 'msg':"rand [" + ','.join(board) + "] " + arg}
+	return {'dir':dir, 'msg':"rand" + gridToString(grid)}
+
+def AIleft(req, grid, score, arg):
+	moveLeft = {'dir':'left', 'msg':"left works " + gridToString(grid)}
+	for row in grid:
+		for tile in range(0, 3):
+			if (row[tile] == 0):
+				if (row[tile+1] != 0):
+					return moveLeft
+			else:
+				if (row[tile] == row[tile+1]):
+					return moveLeft
+	moveUp = {'dir':'up', 'msg':"up works " + gridToString(grid)}
+	for col in range(0, 4):
+                for row in range(0, 3):
+                        if (grid[row][col] == 0):
+                                if (grid[row+1][col] != 0):
+                                        return moveUp
+                        else:
+                                if (grid[row][col] == grid[row+1][col]):
+                                        return moveUp
+	dir = Directions[randint(1, 2)]
+	return {'dir':dir, 'msg':"left and up don't work " + gridToString(grid)}
