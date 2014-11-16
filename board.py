1078bc9b777bdc27f0ac23eb1606fa41e96e1c28
amazingcaleb <calebnelson@gmail.com>
2014-11-16 00:01:59 -0800

    Finished move function
    The huge amount of additions/subtractions is cause I messed with the
    indentation in IDLE

--- board.py
+++ board.py
@@ -1,90 +1,113 @@
 from types import *
 class Board:
-	board  = []
+    board = []
 
-	def __init__(self, grid = None):
-		self.board = []
-		if (grid == None):
-			grid = "2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0"
-		if (type(grid) is ListType):
-			self.board = grid
-		else:
-			grid = map(int, grid.replace(' ', '').split(','))
-			if len(grid) < 16:
-				grid.extend([0 for x in range(16-len(grid))])
-			for row in range(0, 4):
-				self.board.append(grid[4*row:4*(row+1)])
+    def __init__(self, grid = None):
+        self.board = []
+        if (grid == None):
+            grid = "2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0"
+        if (type(grid) is ListType):
+            self.board = grid
+        else:
+            grid = map(int, grid.replace(' ', '').split(','))
+            if len(grid) < 16:
+                grid.extend([0 for x in range(16-len(grid))])
+            for row in range(0, 4):
+                self.board.append(grid[4*row:4*(row+1)])
 
-	def __str__(self):
-	    rowStrs = []
-	    for row in self.board:
-	      rowStrs.append(','.join(map(str, row)))
-	    return ("[" + '|'.join(rowStrs) + "]")
+    def __str__(self):
+      rowStrs = []
+      for row in self.board:
+       rowStrs.append(','.join(map(str, row)))
+      return ("[" + '|'.join(rowStrs) + "]")
 
-	def moveOk(self, r0, c0, r1, c1):
-		a = self.board[r0][c0]
-		b = self.board[r1][c1]
-		return (a == 0 and b != 0) or (a != 0 and a == b)
+    def moveOk(self, r0, c0, r1, c1):
+        a = self.board[r0][c0]
+        b = self.board[r1][c1]
+        return (a == 0 and b != 0) or (a != 0 and a == b)
 
-	def tryDir(self, dir):
-		if (dir == 'l' or dir == "left"):
-			for row in range(0,4):
-				for col in range(0, 3):
-					if (self.moveOk(row, col, row, col+1)):
-						return True
-		elif (dir == 'u' or dir == "up"):
-			for col in range(0, 4):
-				for row in range(0, 3):
-					if (self.moveOk(row, col, row+1, col)):
-						return True
-		elif (dir == 'r' or dir == "right"):
-			for row in range(0,4):
-				for col in range(1, 4):
-					if (self.moveOk(row, col, row, col-1)):
-						return True
-		elif (dir == 'd' or dir == "down"):
-			for col in range(0, 4):
-				for row in range(1, 4):
-					if (self.moveOk(row, col, row-1, col)):
-						return True
-		return False
+    def tryDir(self, dir):
+        if (dir == 'l' or dir == "left"):
+            for row in range(0,4):
+                for col in range(0, 3):
+                    if (self.moveOk(row, col, row, col+1)):
+                        return True
+        elif (dir == 'u' or dir == "up"):
+            for col in range(0, 4):
+                for row in range(0, 3):
+                    if (self.moveOk(row, col, row+1, col)):
+                        return True
+        elif (dir == 'r' or dir == "right"):
+            for row in range(0,4):
+                for col in range(1, 4):
+                    if (self.moveOk(row, col, row, col-1)):
+                        return True
+        elif (dir == 'd' or dir == "down"):
+            for col in range(0, 4):
+                for row in range(1, 4):
+                    if (self.moveOk(row, col, row-1, col)):
+                        return True
+        return False
 
-	def move(self, dir):
-		newboard = Board(self.board)
-		if (not self.tryDir(dir)):
-			return newboard
-		else:
-			if (dir == 'l' or dir == "left"):
-				for row in range(0,4):
-					for c in range(0, 3):
-						col = 2 - c
-						if (newboard.moveOk(row, col, row, col+1)):
-							newboard.board[row][col] = newboard.board[row][col] + newboard.board[row][col+1]
-							newboard.board[row][col+1] = 0
-			elif (dir == 'u' or dir == "up"):
-				for col in range(0, 4):
-					for r in range(0, 3):
-						row = 3 - r
-						if (self.moveOk(row, col, row+1, col)):
-							newboard.board[row][col] = self.board[row][col] + self.board[row+1][col]
-                                                        newboard.board[row+1][col] = 0
-			elif (dir == 'r' or dir == "right"):
-				for row in range(0,4):
-					for col in range(1, 4):
-						if (self.moveOk(row, col, row, col-1)):
-                                                        newboard.board[row][col] = self.board[row][col] + self.board[row][col-1]
-                                                        newboard.board[row][col-1] = 0
-			elif (dir == 'd' or dir == "down"):
-				for col in range(0, 4):
-					for row in range(1, 4):
-						if (self.moveOk(row, col, row-1, col)):
-                                                        newboard.board[row][col] = self.board[row][col] + self.board[row-1][col]
-                                                        newboard.board[row-1][col] = 0
-			return newboard
+    def move(self, dir):
+        newboard = Board(self.board)
+        if (not self.tryDir(dir)):
+            return newboard
+        else:
+            if (dir == 'l' or dir == "left"):
+                for row in range(0,4):
+                    newboard.board[row] = makeMove(newboard.board[row])
+            elif (dir == 'u' or dir == "up"):
+                for col in range(0, 4):
+                    vector = []
+                    for row in range(0, 4):
+                        vector.append(newboard.board[row][col])
+                    vector = makeMove(vector)
+                    for row in range(0, 4):
+                        newboard.board[row][col] = vector[row]
+            elif (dir == 'r' or dir == "right"):
+                for row in range(0,4):
+                    vector = []
+                    for col in range(0, 4):
+                        vector.append(newboard.board[row][3-col])
+                    vector = makeMove(vector)
+                    for col in range(0, 4):
+                        newboard.board[row][3-col] = vector[col]
+            elif (dir == 'd' or dir == "down"):
+                for col in range(0, 4):
+                    vector = []
+                    for row in range(0, 4):
+                        vector.append(newboard.board[3-row][col])
+                    vector = makeMove(vector)
+                    for row in range(0, 4):
+                        newboard.board[3-row][col] = vector[row]
+            return newboard
 
+def squishZeros(vector):
+    index = 0
+    for i in range(0, len(vector)):
+        if (vector[i] != 0):
+            vector[index] = vector[i]
+            if (i != index):
+                vector[i] = 0
+            index += 1
+    return vector
+
+def makeFusions(vector):
+    for i in range(0, len(vector)-1):
+        if (vector[i] == vector[i+1]):
+            vector[i] += vector[i+1]
+            vector[i+1] = 0
+    return vector
+
+def makeMove(vector):
+    vector = squishZeros(vector)
+    vector = makeFusions(vector)
+    vector = squishZeros(vector)
+    return vector
 
 if __name__ == "__main__":
-	testb = Board("2,2,0,0, 0,2,0,4, 0,0,0,0, 0,0,0,0")
-	print testb.tryDir("left")
-	print str(testb)
-	print str(testb.move("left"))
+    testb = Board("2,2,0,0, 0,2,0,4, 0,0,0,0, 0,0,0,0")
+    print testb.tryDir("left")
+    print str(testb)
+    print str(testb.move("r"))
