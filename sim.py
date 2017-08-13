f18a024728a6ead3b3a70d51d7c4ff122c29800d
Caleb Nelson <calebnelson@gmail.com>
2017-08-12 19:16:44 -0700

    Changed output of sim to stdout

--- sim.py
+++ sim.py
@@ -3,12 +3,12 @@ from aibasic import AiRand, AiLeft
 from aiquality import AiQuality
 from aiapi import AiRequest, AiResponse
 from board import Board
+import sys
 
 if __name__ == "__main__":
 	aireq = AiRequest({'board':"2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0", 'gid':1, 'prior':""})
 	moveNum = 0
 	lines = []
-	f = open(str(aireq.gid)+".txt", "w+")
 	while (True):
 		moveNum += 1
 		airesp = AiQuality(aireq)
@@ -19,6 +19,4 @@ if __name__ == "__main__":
 			break
 	for i in range(0, len(lines)):
 		lines[i] = lines[i] + "," + str(len(lines)-(i+1)) + "," + str(aireq.b.score) + "\n"
-		f.write(lines[i])
-	f.write(str(moveNum+1) + "," + str(aireq.b) + ",final")
-	f.close()
+		sys.stdout.write(lines[i])
