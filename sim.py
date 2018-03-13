ee996bfadf10a2527008516251822ff5634dcde5
Caleb Nelson <calebnelson@gmail.com>
2018-03-13 12:47:51 -0700

    changed the way sim reports to fit RL architecture

--- sim.py
+++ sim.py
@@ -1,27 +1,28 @@
-from ailookahead import AiLookahead
-from aibasic import AiRand, AiLeft
-from aiquality import AiQuality
-from aitensorflow import AiTensorFlow
-from aiapi import AiRequest, AiResponse
-from board import Board
-from quality import Model
-import sys
+from aiscore import AiScore
+#from aiquality import AiQuality
+from aiapi import AiRequest
+#from quality import Model
+#import sys
 
 if __name__ == "__main__":
-    m = Model("mnist-model.meta")
-    print "model loaded"
 	aireq = AiRequest({'board':"2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0", 'gid':1, 'prior':""})
 	moveNum = 0
+	score = 0
+	scoreDiff = 0
 	lines = []
 	while (True):
 		moveNum += 1
-        #sys.stdout.write(str(moveNum) + str(aireq.b))
-		airesp = AiQuality(aireq, m)
+		airesp = AiScore(aireq)
 		aireq.b = aireq.b.move(airesp.dir)
-		lines.append(str(moveNum) + "," + str(aireq.b) + "," + str(airesp.dir))
+		scoreDiff = aireq.b.score - score
+		score = aireq.b.score
+		line = str(aireq.b.board) + "," + str(airesp.dir) + "," + str(scoreDiff)
 		aireq.b = aireq.b.drop()
+		line = line + "," + str(aireq.b.board) + "," + str(1 if aireq.b.canMove() else 0)
+		lines.append(line)
 		if (aireq.b.canMove() == False):
 			break
 	for i in range(0, len(lines)):
-		lines[i] = lines[i] + "," + str(len(lines)-(i+1)) + "," + str(aireq.b.score) + "\n"
-		sys.stdout.write(lines[i])
+		#lines[i] = lines[i] + "," + str(len(lines)-(i+1)) + "," + str(aireq.b.score) + "\n"
+		#sys.stdout.write(lines[i])
+		print lines[i]
